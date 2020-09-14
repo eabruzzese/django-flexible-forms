d84022f49a49cbea64230dab2c9948386085c127
Eric Abruzzese <eric.abruzzese@gmail.com>
2020-09-14 15:15:41 -0400

    Versioning setup

--- flexible_forms/__init__.py
+++ flexible_forms/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
+from importlib.metadata import version
+
 default_app_config = "flexible_forms.apps.FlexibleFormsConfig"
+
+try:
+    __version__ = version(__name__)
+except:
+    pass
